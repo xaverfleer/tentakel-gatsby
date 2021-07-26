b66e427720a953c51e895e629a84f870a4d33f17
Xaver Fleer <xaver.fleer@gmail.com>
2021-07-26 23:03:28 +0200

    Add first draft

---
+++ src/pages/screenshot.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import img from "../images/screenshot.jpg";
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={{ textAlign: "center" }}>
+      <title>Tentakel | Erster Entwurf</title>
+      <img alt="Tentakel Entwurf Screenshot" src={img} />
+    </main>
+  );
+};
+
+export default IndexPage;
