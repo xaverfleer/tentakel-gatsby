35671fc330627612e0b5066d2b4ba6db281c203e
Xaver Fleer <xaver.fleer@gmail.com>
2021-08-12 16:56:53 +0200

    Update screenshots

--- src/pages/screenshot.js
+++ src/pages/screenshot.js
@@ -1,12 +1,17 @@
 import * as React from "react";
-import img from "../images/screenshot.jpg";
+import firstDraft from "../images/screenshot.jpg";
+import secondDraft from "../images/screenshot-2021-07-30.jpg";
 
 // markup
 const IndexPage = () => {
   return (
     <main style={{ textAlign: "center" }}>
-      <title>Tentakel | Erster Entwurf</title>
-      <img alt="Tentakel Entwurf Screenshot" src={img} />
+      <title>Tentakel | Entwürfe</title>
+      <h1>Entwürfe</h1>
+      <h2>Zweiter Entwurf - 30. 7. 2021</h2>
+      <img alt="Tentakel Entwurf Screenshot" src={secondDraft} />
+      <h2>Erster Entwurf</h2>
+      <img alt="Tentakel Entwurf Screenshot" src={firstDraft} />
     </main>
   );
 };
