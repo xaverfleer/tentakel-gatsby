7b1de6df10c63b296ea4071a680c8850be72f64e
Xaver Fleer <xaver.fleer@gmail.com>
2021-08-15 12:04:55 +0200

    Add netlify-cms

---
+++ src/pages/admin/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <>
+      <title>Content Manager</title>
+      {/* <html lang="de"> */}
+      {/* <head> */}
+      {/* <meta charset="utf-8" /> */}
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      {/* </head> */}
+      {/* <body> */}
+      <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+      {/* </body> */}
+      {/* </html> */}
+    </>
+  );
+};
+
+export default AdminPage;
