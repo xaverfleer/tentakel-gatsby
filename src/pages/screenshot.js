cddb6837a81d3314acb806f1cd5a0e6f4c98b426
Xaver Fleer <xaver.fleer@gmail.com>
2021-08-17 16:56:12 +0200

    Add artikel elemente to /screenshot

--- src/pages/screenshot.js
+++ src/pages/screenshot.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import firstDraft from "../images/screenshot.jpg";
 import secondDraft from "../images/screenshot-2021-07-30.jpg";
 import thirdDraft from "../images/screenshot-2021-08-13.pdf";
+import artikelElemente from "../images/artikel-elemente.jpg";
 
 // markup
 const IndexPage = () => {
@@ -9,7 +10,9 @@ const IndexPage = () => {
     <main style={{ textAlign: "center" }}>
       <title>Tentakel | Entwürfe</title>
       <h1>Entwürfe</h1>
-      <h2>Dritter Entwurf – 13. 8. 2021</h2>
+      <h2>Artikel Elemente – 14. 8. 2021</h2>
+      <img alt="Artikel Elemente" src={artikelElemente} />
+      <h2>Dritter Entwurf – 13. 8. 2021</h2>
       <a href={thirdDraft}>PDF</a>
       <h2>Zweiter Entwurf - 30. 7. 2021</h2>
       <img alt="Tentakel Entwurf Screenshot" src={secondDraft} />
