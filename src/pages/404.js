0335bea0951a72a685e4c15fa5ccdc480b998972
Xaver Fleer <xaver.fleer@gmail.com>
2021-08-31 10:30:02 +0200

    improve 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,26 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "gatsby";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from "../components/Layout";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <main>
+      <Helmet>
+        <title>Tentakel | Seite nicht gefunden</title>
+      </Helmet>
+      <h1>Seite nicht gefunden</h1>
+      <p>
+        Sorry, Die gesuchte Seite ist entweder temporär nicht verfügbar, wurde
+        an einen anderen Ort verschoben oder existiert nicht mehr.
+      </p>
+
+      <p>
+        <Link to="/">Zur Startseite</Link>.
       </p>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
