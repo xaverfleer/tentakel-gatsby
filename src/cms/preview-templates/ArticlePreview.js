bcefe1398e8bfdc554e9dffde7269976e0b5bf3a
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-17 22:15:03 +0200

    Fix ArticlePreview data formatting

--- src/cms/preview-templates/ArticlePreview.js
+++ src/cms/preview-templates/ArticlePreview.js
@@ -3,21 +3,19 @@ import Article from "../../templates/Article";
 export default ({ entry }) => {
   const data = entry.getIn(["data"]).toJS();
 
-  debugger;
-
   return Article({
-    article: {
-      frontmatter: {
-        title: data.title,
-        datetime: data.datetime,
-        author: data.author,
-        category: data.category,
-        datetime: data.datetime,
-        pic: data.pic,
-        title: data.title,
-        teaser: data.teaser,
+    data: {
+      article: {
+        frontmatter: {
+          title: data.title,
+          author: data.author,
+          category: data.category,
+          datetime: data.datetime,
+          pic: data.pic,
+          teaser: data.teaser,
+        },
+        html: data.body,
       },
-      html: data.html,
     },
   });
 };
