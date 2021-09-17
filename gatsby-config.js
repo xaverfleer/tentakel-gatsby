7cd64b53143079f948add04f8ae54aede27db17b
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-17 21:00:16 +0200

    Try to add custom preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,9 @@ module.exports = {
       },
     },
     "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: { modulePath: `${__dirname}/src/cms/cms.js` },
+    },
   ],
 };
---
+++ src/cms/cms.js
@@ -0,0 +1,7 @@
+import React from "react";
+import CMS from "netlify-cms-app";
+
+console.log("\n\n\n\nIn the browser: passed at /src/cms/cms.min.js\n\n\n\n\n");
+debugger;
+CMS.init();
+CMS.registerPreviewTemplate("artikel", () => <div>hello</div>);
