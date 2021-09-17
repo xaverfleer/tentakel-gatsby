def6972f50debbb2f10bdb24f0fd6cbe649889a2
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-17 21:21:33 +0200

    Remove CMS.init()

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,5 +3,4 @@ import CMS from "netlify-cms-app";
 
 console.log("\n\n\n\nIn the browser: passed at /src/cms/cms.min.js\n\n\n\n\n");
 debugger;
-CMS.init();
 CMS.registerPreviewTemplate("artikel", () => <div>hello</div>);
