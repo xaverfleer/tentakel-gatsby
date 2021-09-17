3b9260b35abae52a3df77acb5a02e39d86d9b26f
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-17 22:28:57 +0200

    Remove debugger statements

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,4 @@
 import CMS from "netlify-cms-app";
 import ArticlePreview from "./preview-templates/ArticlePreview";
 
-console.log("\n\n\n\nIn the browser: passed at /src/cms/cms.min.js\n\n\n\n\n");
-debugger;
 CMS.registerPreviewTemplate("artikel", ArticlePreview);
