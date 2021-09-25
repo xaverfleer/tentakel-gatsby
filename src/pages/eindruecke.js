f20028bca00e1b7fa7e1bdc2dfbabd0a3f7958a2
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-25 22:12:47 +0200

    Add /eindruecke draft

---
+++ src/pages/eindruecke.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+
+import Layout from "../components/Layout";
+
+import { isoDateToGermanDate } from "../modules/dataConverter";
+
+const EindrueckePage = ({ data }) => {
+  const { nodes } = data.allMarkdownRemark;
+  const eindruecke = nodes.map((n) => n.frontmatter);
+
+  return (
+    <>
+      <Helmet>
+        <title>Tentakel | Eindrücke</title>
+      </Helmet>
+      <Layout>
+        <main className="main">
+          {eindruecke.map((e) => (
+            <img src={e.pic} />
+          ))}
+        </main>
+      </Layout>
+    </>
+  );
+};
+
+export default EindrueckePage;
+
+export const pageQuery = graphql`
+  query eindruecke {
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "eindruck" } } }
+    ) {
+      nodes {
+        frontmatter {
+          datetime
+          title
+          date
+          event
+          pic
+          location
+        }
+      }
+    }
+  }
+`;
